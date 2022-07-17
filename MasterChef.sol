6f136545fa21c98287fe115b33d7ab25c59541d5
pavel.soloviev <pavel.soloviev@latoken.com>
2022-07-17 13:01:55 +0300

    ZAP

--- MasterChef.sol
+++ MasterChef.sol
@@ -6,6 +6,9 @@ import "./openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "./openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./openzeppelin/contracts/access/UpgradableOwnable.sol";
+import "./interfaces/IUniswapV2Router.sol";
+import "./interfaces/IUniswapV2Pair.sol";
+import "./libraries/Babylonian.sol";
 import "./utils/Proxy.sol";
 
 interface IMigratorChef {
@@ -19,6 +22,11 @@ interface IMigratorChef {
     // else something bad will happen. Traditional UniswapV2 does not
     // do that so be careful!
     function migrate(IERC20 token) external returns (IERC20);
+}
+
+interface IWETH is IERC20 {
+    function deposit() external payable;
+    function withdraw(uint256 wad) external;
 }
 
 // MasterChef is the master of Sushi. He can make Sushi and he is a fair guy.
@@ -75,6 +83,10 @@ contract MasterChef is Storage, UpgradableOwnable {
     uint256 public totalAllocPoint = 0;
     // The block number when SUSHI mining starts.
     uint256 public startBlock;
+    // Wrapped native token address
+    IWETH public WETH;
+    // Ladex router
+    IUniswapV2Router public router;
     event Add(uint256 indexed pid, address lpToken, address rewardToken, uint256 allocPoint, uint256 rewardPerBlock);
     event Set(uint256 indexed pid, uint256 allocPoint, uint256 rewardPerBlock);
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
@@ -258,6 +270,14 @@ contract MasterChef is Storage, UpgradableOwnable {
 
     // Deposit LP tokens to MasterChef for SUSHI allocation.
     function deposit(uint256 _pid, uint256 _amount) public {
+        depositInternal(_pid, _amount, address(0));
+    }
+
+    function depositSingleToken(uint256 _pid, uint256 _amount, address _depositToken) public payable {
+        depositInternal(_pid, _amount, _depositToken);
+    }
+
+    function depositInternal(uint256 _pid, uint256 _amount, address _depositToken) internal {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
@@ -269,11 +289,30 @@ contract MasterChef is Storage, UpgradableOwnable {
             
             safeSushiTransfer(msg.sender, pending, address(pool.rewardToken));
         }
-        pool.lpToken.transferFrom(
-            address(msg.sender),
-            address(this),
-            _amount
-        );
+
+        if (_depositToken == address(0)) {
+            pool.lpToken.transferFrom(
+                msg.sender,
+                address(this),
+                _amount
+            );
+        } else {
+            uint256 beforeLpTokenBalance = pool.lpToken.balanceOf(address(this));
+
+            if (address(_depositToken) == address(WETH)) {
+                WETH.deposit{value: _amount}();
+            } else {
+                IERC20(_depositToken).transferFrom(
+                    msg.sender,
+                    address(this),
+                    _amount
+                );
+            }
+            zap(IUniswapV2Pair(pool.lpToken), address(_depositToken), _amount, true);
+
+            _amount = pool.lpToken.balanceOf(address(this)).sub(beforeLpTokenBalance);
+        }
+
         user.amount = user.amount.add(_amount);
         user.rewardDebt = user.amount.mul(pool.accSushiPerShare).div(1e12);
         emit Deposit(msg.sender, _pid, _amount);
@@ -281,6 +320,15 @@ contract MasterChef is Storage, UpgradableOwnable {
 
     // Withdraw LP tokens from MasterChef.
     function withdraw(uint256 _pid, uint256 _amount) public {
+        withdrawInternal(_pid, _amount, address(0));
+    }
+
+    // Withdraw LP tokens from MasterChef and swap them for single token.
+    function withdrawSingleToken(uint256 _pid, uint256 _amount, address _depositToken) public {
+        withdrawInternal(_pid, _amount, _depositToken);
+    }
+
+    function withdrawInternal(uint256 _pid, uint256 _amount, address _depositToken) internal {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
@@ -292,7 +340,13 @@ contract MasterChef is Storage, UpgradableOwnable {
         safeSushiTransfer(msg.sender, pending, address(pool.rewardToken));
         user.amount = user.amount.sub(_amount);
         user.rewardDebt = user.amount.mul(pool.accSushiPerShare).div(1e12);
-        pool.lpToken.transfer(address(msg.sender), _amount);
+        
+        if (_depositToken == address(0)) {
+            pool.lpToken.transfer(msg.sender, _amount);
+        } else {
+            zap(IUniswapV2Pair(pool.lpToken), _depositToken, _amount, false);
+        }
+
         emit Withdraw(msg.sender, _pid, _amount);
     }
 
@@ -337,5 +391,96 @@ contract MasterChef is Storage, UpgradableOwnable {
     function dev(address _devaddr) public {
         require(msg.sender == devaddr, "dev: wut?");
         devaddr = _devaddr;
+    }
+
+    function setWETH(address _wethAddress) public onlyOwner {
+        WETH = IWETH(_wethAddress);
+    }
+
+    function setRouter(address _routerAddress) public onlyOwner {
+        router = IUniswapV2Router(_routerAddress);
+    }
+
+    function zap(
+        IUniswapV2Pair _pair,
+        address _depositToken,
+        uint256 _amount,
+        bool _isDeposit
+    ) internal {
+        bool isWant0 = _pair.token0() == _depositToken;
+        require(isWant0 || _pair.token1() == _depositToken, 'Desired token not present in liquidity pair');
+
+        address[] memory path = new address[](2);
+        uint256 beforeDepositTokenBalance = IERC20(_depositToken).balanceOf(address(this));
+
+        if (_isDeposit) {
+            path[0] = _depositToken;
+            path[1] = address(isWant0 ? _pair.token1() : _pair.token0());
+
+            uint256 swapAmountIn;
+            (uint256 reserveA, uint256 reserveB,) = _pair.getReserves();
+            if (isWant0) {
+                swapAmountIn = getSwapAmount(_amount, reserveA, reserveB);
+            } else {
+                swapAmountIn = getSwapAmount(_amount, reserveB, reserveA);
+            }
+
+            uint256[] memory swappedAmounts = swapExactTokensForTokens(swapAmountIn, 1, path, address(this), block.timestamp);
+
+            IERC20(path[0]).approve(address(router), _amount.sub(swappedAmounts[0]));
+            IERC20(path[1]).approve(address(router), swappedAmounts[1]);
+            (,,) = router.addLiquidity(path[0], path[1], _amount.sub(swappedAmounts[0]), swappedAmounts[1], 1, 1, address(this), block.timestamp);
+        
+            beforeDepositTokenBalance = beforeDepositTokenBalance.sub(_amount);
+        } else {
+            path[0] = address(isWant0 ? _pair.token1() : _pair.token0());
+            path[1] = _depositToken;
+
+            IERC20(_pair).transfer(address(_pair), _amount);
+            (uint256 amount0, uint256 amount1) = IUniswapV2Pair(_pair).burn(address(this));
+
+            swapExactTokensForTokens(isWant0 ? amount1 : amount0, 1, path, address(this), block.timestamp);
+        }
+
+        returnAsset(_depositToken, IERC20(_depositToken).balanceOf(address(this)).sub(beforeDepositTokenBalance), msg.sender);
+    }
+
+    function swapExactTokensForTokens(
+        uint _amountIn,
+        uint _amountOutMin,
+        address[] memory _path,
+        address _to,
+        uint _deadline
+    ) internal returns (uint[] memory) {
+        IERC20(_path[0]).approve(address(router), _amountIn);
+        return router.swapExactTokensForTokens(_amountIn, _amountOutMin, _path, _to, _deadline);
+    }
+
+    function returnAsset(
+        address _token,
+        uint256 _amount,
+        address _to
+    ) internal {
+        if (_amount > 0) {
+            if (_token == address(WETH)) {
+                WETH.withdraw(_amount);
+                payable(_to).send(_amount);
+            } else {
+                IERC20(_token).transfer(_to, _amount);
+            }
+        }
+    }
+
+    function getSwapAmount(
+        uint256 _investmentA,
+        uint256 _reserveA,
+        uint256 _reserveB
+    ) internal view returns (uint256 swapAmount) {
+        uint256 halfInvestment = _investmentA / 2;
+
+        uint256 nominator = router.getAmountOut(halfInvestment, _reserveA, _reserveB);
+        
+        uint256 denominator = router.quote(halfInvestment, _reserveA.add(halfInvestment), _reserveB.sub(nominator));
+        swapAmount = _investmentA.sub(Babylonian.sqrt(halfInvestment * halfInvestment * nominator / denominator));
     }
 }
