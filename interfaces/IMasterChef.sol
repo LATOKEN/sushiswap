107ad4db2329d0e097c13c6f2fbcb92dcd521559
Pavel Soloviev <pavel.soloviev@latoken.com>
2022-08-16 03:48:29 +0300

    Added methods to IMasterChef

--- interfaces/IMasterChef.sol
+++ interfaces/IMasterChef.sol
@@ -33,7 +33,7 @@ interface IMasterChef {
 
     function claim(uint256 _pid) external;
 
-    function withdrawSingleToken(uint256 _pid, uint256 _amount, address _depositToken) external;
+    function withdrawSingleToken(uint256 _pid, uint256 _amount, address _depositToken) external payable;
 
     function setSushiPerBlock(uint256 _sushiPerBlock) external;
 
@@ -42,6 +42,16 @@ interface IMasterChef {
     function setWETH(address _WETH) external;
 
     function setRouter(address _router) external;
+
+    function sushiPerBlock() external view returns (uint256);
+
+    function zapLadex() external view returns (address);
+    
+    function fee() external view returns (uint256);
+    
+    function WETH() external view returns (address);
+    
+    function router() external view returns (address);
 
     function add(
         uint256 _allocPoint,
@@ -61,5 +71,6 @@ interface IMasterChef {
 
     function poolInfo(uint256 pid) external view returns (IMasterChef.PoolInfo memory);
     function totalAllocPoint() external view returns (uint256);
-    function deposit(uint256 _pid, uint256 _amount) external;
+    function deposit(uint256 _pid, uint256 _amount) external payable;
+    function withdraw(uint256 _pid, uint256 _amount) external payable;
 }
